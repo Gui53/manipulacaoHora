9f45b4a7344f76a6e064fa0ff4284274986faab8
Gui53 <gui53tube@gmail.com>
2026-04-07 18:32:17 -0300

    Feat: Função de decrementar tempo

--- src/main/java/main/ManipulacaoHora.java
+++ src/main/java/main/ManipulacaoHora.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package main;
 
 import java.util.Scanner;
@@ -14,22 +13,36 @@ import model.Relogio;
 public class ManipulacaoHora {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        
+
         Relogio relogio = new Relogio();
-        
+
         System.out.println("Digite os segundos");
         relogio.setSegundo(Integer.parseInt(sc.nextLine()));
-        
+
         System.out.println("Digite os minutos");
         relogio.setMinuto(Integer.parseInt(sc.nextLine()));
-        
+
         System.out.println("Digite as horas");
         relogio.setHora(Integer.parseInt(sc.nextLine()));
-        
+
         relogio.incrementaTempo();
+
+        System.out.println(relogio.formato());
+
+        System.out.println("Digite as horas que você quer decrementar:");
+        relogio.setDecrementaHora(Integer.parseInt(sc.nextLine()));
+
+        System.out.println("Digite os minutos que você quer decrementar:");
+        relogio.setDecrementaMinuto(Integer.parseInt(sc.nextLine()));
+
+        System.out.println("Digite os segundos que você quer decrementar:");
+        relogio.setDecrementaSegundo(Integer.parseInt(sc.nextLine()));
+
         
+        relogio.decrementaTempo();
+
         System.out.println(relogio.formato());
     }
 }
